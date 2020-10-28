26efdf7c19fa759a3fd8cc9d329021df8969b74c
Mai Pham <maitphm@gmail.com>
2020-10-28 02:39:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,10 +32,10 @@ def monopoly_with_third_tier
       },
       :names=> 
       { 
-        :reading_railroad=>{}
-        :pennsylvania_railroad=>{}
-        :b_and_o_railroad=>{}
-        :shortline_railroad=>{} 
+        :reading_railroad =>{}
+        :pennsylvania_railroad =>{}
+        :b_and_o_railroad =>{}
+        :shortline_railroad =>{} 
       }
     }
   }
