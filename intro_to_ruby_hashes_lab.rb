af454f83752375b14b06c0ffc6ea5259e7c6237f
Mai Pham <maitphm@gmail.com>
2020-10-28 02:17:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,11 +6,11 @@ end
 def monopoly_with_second_tier
 
   hash = {
-    :railroads => 
+    :railroads=> 
     {
-      :pieces => 4,
-      :rent_in_dollars => 
-      :names => 
+      :pieces=> 4,
+      :rent_in_dollars=> 
+      :names=> 
     }
   }
 end
@@ -18,7 +18,7 @@ end
 def monopoly_with_third_tier
   
   hash = {
-    :railroads => 
+    :railroads=> 
     {
       :pieces => 4,
       :rent_in_dollars => 
